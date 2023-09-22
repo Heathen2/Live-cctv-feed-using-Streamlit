4956cb14172390f8825080a7e38629fc488dccb9
Athul <34716154+Heathen2@users.noreply.github.com>
2023-09-22 15:04:18 +0530

    Updated for Multiple Camera feeds

--- camera_live.py
+++ camera_live.py
@@ -1,15 +1,16 @@
 import streamlit as st
+import cv2
 
 st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
+    page_title="Live CCTV Feed",
     page_icon="🧊",
     layout="wide",
     initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
+    # menu_items={
+    #     'Get Help': 'https://www.extremelycoolapp.com/help',
+    #     'Report a bug': "https://www.extremelycoolapp.com/bug",
+    #     'About': "# This is a header. This is an *extremely* cool app!"
+    # }
 )
 
 col1, col2 = st.columns(2)
@@ -19,15 +20,29 @@ col3, col4 = st.columns(2)
 # video_bytes = video_file.read()
 
 # st.video(video_bytes)
-esp_cam_url = "http://192.168.1.101:81/stream"
 
 with col1:
-   st.image(esp_cam_url,caption='cam #1')
-
+   cam1 = st.text_input('cam #1', 'http://192.168.1.101:81/stream',key="cam1",label_visibility="collapsed",placeholder="cam #1 url")
+   try:
+      cam1_feed = st.image(cam1,use_column_width =True)
+   except:
+      st.image("Original-colour-bar.png")
 with col2:
-   st.image(esp_cam_url,caption='cam #2')
+   cam2 = st.text_input('cam #2', 'http://192.168.1.101:81/stream',key="cam2",label_visibility="collapsed",placeholder="cam #2 url")
+   try:
+      st.image(cam2,use_column_width =True)
+   except:
+      st.image("Original-colour-bar.png")
 
 with col3:
-   st.image(esp_cam_url,caption='cam #3')
+   cam3 = st.text_input('cam #3', 'http://192.168.1.101:81/stream',key="cam3",label_visibility="collapsed",placeholder="cam #3 url")
+   try:
+      st.image(cam3,use_column_width =True)
+   except:
+      st.image("Original-colour-bar.png")
 with col4:
-   st.image(esp_cam_url,caption='cam #4')
+   cam4 = st.text_input('cam #4', 'http://192.168.1.101:81/stream',key="cam4",label_visibility="collapsed",placeholder="cam #4 url")
+   try:
+      st.image(cam4,use_column_width =True)
+   except:
+      st.image("Original-colour-bar.png")
